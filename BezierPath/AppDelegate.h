2c96db741b2d7523b5e106861c510d8d72418438
shumei <shumei@shumei.local>
2016-08-05 15:33:59 +0800

    Initial Commit

---
+++ BezierPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BezierPath
+//
+//  Created by shumei on 16/8/5.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BezierPath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BezierPath
+//
+//  Created by shumei on 16/8/5.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
